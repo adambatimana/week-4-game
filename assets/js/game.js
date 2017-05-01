416af1d02d6de118b85270899c2a8f0b1adebc41
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-01 16:05:08 -0400

    functional game

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,22 +9,22 @@ var loseDisplay = $(".loss");
 var totalDisplay = $(".totalScore");
 var win = 0;
 var loss = 0;
+var totalScore = 0;
+var numbers = [0];
 var triVal;
 var rubyVal;
 var diaVal;
 var squareVal;
 var compNum;
 
-//set random number between 19 - 120 for computer
+// ***************************** FUNCTIONS ****************************
 function selectRandomNum (){
+  //set random number between 19 - 120 for computer
   compNum = Math.floor((Math.random() * 101) + 19);
+  //check function
+  console.log(compNum);
 }
-selectRandomNum();
-compScore.html(compNum);
-winDisplay.html(win);
-loseDisplay.html(loss);
 
-console.log(compNum);
 
 //set random number btw 1 -12 for each crystal button set once then reset during restart
 function selectRandomVal(){
@@ -32,23 +32,79 @@ function selectRandomVal(){
   rubyVal = Math.floor((Math.random() * 12) + 1);
   diaVal = Math.floor((Math.random() * 12) + 1);
   squareVal = Math.floor((Math.random() * 12) + 1);
+  //check function
+  console.log(triVal);
+  console.log(rubyVal);
+  console.log(diaVal);
+  console.log(squareVal);
 }
+
+
+//find sum off array of numbers
+function getSum(total, num){
+  return total + num;
+}
+
+//check win or lose
+function checkWin(){
+  if (compNum === numbers.reduce(getSum)) {
+    alert("YOU WIN!")
+    win++;
+    //send win number to html
+    winDisplay.html(win);
+    newGame();
+  }
+  if (numbers.reduce(getSum)> compNum) {
+    alert("YOU LOSE!")
+    loss++;
+    //send loss number to html
+    loseDisplay.html(loss);
+    newGame();
+  }
+}
+function newGame(){
+  selectRandomNum();
+  selectRandomVal();
+  compScore.html(compNum);
+  totalScore = 0;
+  totalDisplay.html(totalScore);
+  numbers = [0];
+}
+// ***************************** INVOKE FUNCTIONS  ****************************
+function gameStart(){
+selectRandomNum();
 selectRandomVal();
-console.log(triVal);
-console.log(rubyVal);
-console.log(diaVal);
-console.log(squareVal);
-
-
-//games start function
-$(document).on('keyup', function (event) {
 //send random comp number to html
-
-//every time a button is pressed add value of that number to total SCORE
-
+compScore.html(compNum);
 //display total score in total field
+totalDisplay.html(totalScore);
+}
+gameStart();
 
-//if total score equals computer gen score then win
-
-//if total score is greater than comp gen then lose
+// ***************************** games start function  ****************************
+$(document).on("click", "button", function () {
+//every time a button is pressed add value of that number to total SCORE
+if ($(this).hasClass("triangle")) {
+  //if button pressed has class of triangle add triangle value to total score
+  numbers.push(triVal);
+  console.log(this);
+}
+if ($(this).hasClass("ruby")) {
+  //if button pressed has class of triangle add triangle value to total score
+  numbers.push(rubyVal);
+  console.log(this);
+}
+if ($(this).hasClass("diamond")) {
+  //if button pressed has class of triangle add triangle value to total score
+  numbers.push(diaVal);
+  console.log(this);
+}
+if ($(this).hasClass("square")) {
+  //if button pressed has class of triangle add triangle value to total score
+  numbers.push(squareVal);
+  console.log(this);
+}
+getSum();
+totalDisplay.html(numbers.reduce(getSum));
+checkWin();
  });
