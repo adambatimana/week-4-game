640381909feb68b9c42612b26d6a62e792c9ead8
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-01 10:11:14 -0400

    pseudo code

---
+++ assets/js/game.js
@@ -0,0 +1,19 @@
+//global variables
+
+//set up variables to be used in the game
+
+//grab html
+
+//global VARIABLES
+
+//games start function
+
+//set random number between 19 - 120 for computer
+
+//set random number btw 1 -12 for each crystal button
+
+//send random comp number to html
+
+//every time a button is pressed add value of that number to total SCORE
+
+//display total score in total field 
